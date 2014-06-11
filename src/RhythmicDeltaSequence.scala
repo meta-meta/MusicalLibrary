df5b91fea4b92238b2e760a67e59ad254c055cf0
meta-meta <paulmchristian@gmail.com>
2014-06-11 18:59:16 -0400

    fix inaccuracy in RhythmicDeltaSequence constuctor require

--- src/RhythmicDeltaSequence.scala
+++ src/RhythmicDeltaSequence.scala
@@ -1,4 +1,6 @@
 case class RhythmicDeltaSequence(name: String, sequence: Vector[Int], rhythm: Vector[Float])
   extends Sequence with Rhythmic with Deltas {
-  require(rhythm.length == sequence.length + 1, "Rhythm length must be 1 less than sequence length")
+
+  require(rhythm.length == sequence.length + 1,
+    "Rhythm length must be 1 more than sequence length. The first note is not part of the sequence.")
 }
