11e85baa4100fa039b18b7b9ffddb8384a6a557e
meta-meta <paulmchristian@gmail.com>
2014-06-11 18:58:34 -0400

    add function to traverse a Note Sequence by way of a Delta Sequence

--- src/Notes.scala
+++ src/Notes.scala
@@ -13,6 +13,29 @@ trait Notes extends Sequence {
     NoteSequence(name, sequence map (n => (n - sequence.head) % 12 + sequence.head))
   }
 
+
+  /**
+   * traverse a note sequence moving to a new index by steps of a delta sequence. useful for creating scale excercises
+   * @param startingIndex defaults to 0
+   * @param deltaSequence
+   * @return
+   */
+  def traverseByDeltaSequence(startingIndex: Int = 0, deltaSequence: Sequence with Deltas): Sequence with Notes = {
+    //TODO: require the sequence stays in bounds of the Note Sequence or option to loop over
+    var i = startingIndex
+    val seq = Vector(sequence(i)) ++
+      (deltaSequence.sequence map (d => {
+        println(i)
+        i += d
+        sequence(i)
+      }))
+    //TODO: map with accumulator instead of side-effect?
+
+    //TODO: match by trait to generate either a NoteSequence or a RhythmicNoteSequence?
+    NoteSequence(name + "TraversedBy" + deltaSequence.name, seq)
+  }
+
+
   protected def getDeltaSequence(noteSequence: Sequence with Notes): Vector[Int] = {
     require(noteSequence.sequence.length > 1, "Sequence length must be greater than 1")
     val deltaList = mutable.MutableList[Int]()
