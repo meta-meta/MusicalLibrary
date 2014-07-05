76bd81d1f6c8717682a81cd7975ad1f3dcc8a0e1
meta-meta <paulmchristian@gmail.com>
2014-07-04 23:17:57 -0500

    add rhythmic constants to Helper trait

--- src/Helper.scala
+++ src/Helper.scala
@@ -3,4 +3,16 @@ trait Helper {
     val tuples = sequences map (s => (s.name + " by " + modulator.name, s.modulateByDeltaSequence(firstNote, modulator)))
     tuples.toMap
   }
+
+  val W = 1f
+  val H = 0.5f
+  val Q = 0.25f
+  val E = 0.125f
+  val S = 0.0625f
+
+  protected def tuplet(n: Int, d: Float) = (d * 2) / n
+
+  val Q3 = tuplet(3, Q)
+  val E3 = tuplet(3, E)
+
 }
